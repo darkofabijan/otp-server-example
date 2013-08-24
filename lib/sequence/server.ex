81ac1eb3ae8ef7e623574d68dd04910cca353f35
Darko Fabijan <darko@renderedtext.com>
2013-08-24 19:29:47 +0200

    Two call handlers example.

--- lib/sequence/server.ex
+++ lib/sequence/server.ex
@@ -9,4 +9,8 @@ defmodule Sequence.Server do
     { :reply, current_number, current_number+1 }
   end
 
+  def handle_call({:set_number, new_number}, _from, _current_number) do
+    { :reply, new_number, new_number }
+  end
+
 end
